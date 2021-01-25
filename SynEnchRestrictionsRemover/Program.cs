89fcc9ec553b406c41460b5e1ca11c15ca96c7a6
Minizbot2012 <mini@zbots.org>
2021-01-25 17:44:37 -0500

    Clean Code, muta/syn update

--- SynEnchRestrictionsRemover/Program.cs
+++ SynEnchRestrictionsRemover/Program.cs
@@ -5,31 +5,36 @@ using Mutagen.Bethesda.Skyrim;
 
 namespace SynEnchRestrictionsRemover
 {
-    public class Program
+    internal class Program
     {
         public static async Task<int> Main(string[] args)
         {
 
-            return await SynthesisPipeline.Instance.AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch).Run(args, new RunPreferences() {
-                    ActionsForEmptyArgs = new RunDefaultPatcher
-                    {
-                        IdentifyingModKey = "EnchRestricts.esp",
-                        TargetRelease = GameRelease.SkyrimSE
-                    }
-                });
+            return await SynthesisPipeline.Instance.AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch).Run(args, new RunPreferences()
+            {
+                ActionsForEmptyArgs = new RunDefaultPatcher
+                {
+                    IdentifyingModKey = "EnchRestricts.esp",
+                    TargetRelease = GameRelease.SkyrimSE
+                }
+            });
         }
 
         public static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
         {
             var formList = state.PatchMod.FormLists.AddNew("NER");
-            foreach(var kywd in state.LoadOrder.PriorityOrder.OnlyEnabled().Keyword().WinningOverrides()) {
+            foreach (var kywd in state.LoadOrder.PriorityOrder.OnlyEnabled().Keyword().WinningOverrides())
+            {
                 var edid = kywd.EditorID;
-                if((edid?.Contains("Clothing") ?? false) || ((edid?.Contains("Armor") ?? false) && (!edid?.Contains("ArmorMaterial") ?? false)) || (edid?.Contains("WeapType") ?? false)) {
+                if ((edid?.Contains("Clothing") ?? false) || ((edid?.Contains("Armor") ?? false) && (!edid?.Contains("ArmorMaterial") ?? false)) || (edid?.Contains("WeapType") ?? false))
+                {
                     formList.Items.Add(kywd.FormKey);
                 }
             }
-            foreach(var ench in state.LoadOrder.PriorityOrder.OnlyEnabled().ObjectEffect().WinningOverrides()) {
-                if(ench.EnchantType != ObjectEffect.EnchantTypeEnum.StaffEnchantment) {
+            foreach (var ench in state.LoadOrder.PriorityOrder.OnlyEnabled().ObjectEffect().WinningOverrides())
+            {
+                if (ench.EnchantType != ObjectEffect.EnchantTypeEnum.StaffEnchantment)
+                {
                     var onch = state.PatchMod.ObjectEffects.GetOrAddAsOverride(ench);
                     onch.WornRestrictions = formList.FormKey;
                 }
